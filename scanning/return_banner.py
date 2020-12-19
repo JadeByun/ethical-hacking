2358d496754f4fe11c9df18543991b6cee5ddfe4
Jimin Byun <jiminbyun@MacBook.local>
2020-12-19 11:48:00 -0800

    fix string format

--- scanning/return_banner.py
+++ scanning/return_banner.py
@@ -8,14 +8,8 @@ def return_banner(host, port):
         sock.connect((host, int(port)))
         sock.settimeout(100)
         banner = sock.recv(1024)
-<<<<<<< HEAD
         print(f'[+] {host}/{port}: {banner}')
-    except:
-=======
-        print('[+] {}/tcp open'.format(port))
-        print(banner)
     except Exception:
->>>>>>> a48c94d650a3b4821857a0686335261c453e27b9
         print('[+] {}/tcp closed'.format(port))
     finally:
         sock.close()
