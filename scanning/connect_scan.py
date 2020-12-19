35c5c9407d79da1ab65b6426a99d98738b72c959
Jimin Byun <jiminbyun@MacBook.local>
2020-12-18 21:25:56 -0800

    convert port type to int

--- scanning/connect_scan.py
+++ scanning/connect_scan.py
@@ -4,7 +4,7 @@ import socket
 def connect_scan(host, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((host, port))
+        sock.connect((host, int(port)))
         sock.settimeout(100)
         print('[+] {}/tcp open'.format(port))
     except:
