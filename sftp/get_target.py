c0a77abebb0d8519da28e7e5d39c8beee26a7d57
JadeByun <jimin.jade.b@gmail.com>
2020-12-19 22:51:02 -0800

    password issue

--- sftp/get_target.py
+++ sftp/get_target.py
@@ -1,21 +1,11 @@
 import argparse
-import getpass
-
-
-class Password(argparse.Action):
-
-    def __call__(self, parser, namespace, values, option_string):
-        if values is None:
-            values = getpass.getpass()
-
-        setattr(namespace, self.dest, values)
 
 
 def get_target():
     parser = argparse.ArgumentParser(description='Usage of program: ' + '-H <SSH host> -u <SSH user> -p <SSH password>')
     parser.add_argument('-H', dest='host', required=True, help='specify SSH host')
     parser.add_argument('-u', dest='user', help='specify SSH user')
-    parser.add_argument('-p', action=Password, dest='password', help='enter a SSH password')
+    parser.add_argument('-p', dest='password', help='enter a SSH password')
 
     args = parser.parse_args()
     host = args.host
