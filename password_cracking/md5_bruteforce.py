46ab3956d7496e68968cf619203113e619f40226
Jimin Byun <jiminbyun@MacBook.local>
2020-12-24 18:37:06 -0800

    functionized modules in password_cracking

--- password_cracking/md5_bruteforce.py
+++ password_cracking/md5_bruteforce.py
@@ -9,17 +9,19 @@ except ImportError:
     sys.path.append(sys.path[0] + '/..')
     from utilities.file_check import file_check
 
-md5_hash = input('[*] Enter MD5 Hash value: ')
-filepath = input('Enter the absolute path of a password list: ')
-password_list = file_check(filepath)
-print('password_list = ', password_list)
 
-for password in password_list.readlines():
-    password = password.strip('\n')
-    print(colored('[-] Trying: ' + password, 'red'))
-    hashed = hashlib.md5(bytes(password, 'utf-8')).hexdigest()
-    if hashed == md5_hash:
-        print(colored('[+] The password is: ' + str(password), 'green'))
-        quit()
+def md5_bruteforce():
+    md5_hash = input('[*] Enter MD5 Hash value: ')
+    filepath = input('Enter the absolute path of a password list: ')
+    password_list = file_check(filepath)
+    print('password_list = ', password_list)
 
-print('Password not in the list')
+    for password in password_list.readlines():
+        password = password.strip('\n')
+        print(colored('[-] Trying: ' + password, 'red'))
+        hashed = hashlib.md5(bytes(password, 'utf-8')).hexdigest()
+        if hashed == md5_hash:
+            print(colored('[+] The password is: ' + str(password), 'green'))
+            quit()
+
+    print('Password not in the list')
--- password_cracking/sha1_bruteforce.py
+++ password_cracking/sha1_bruteforce.py
@@ -3,15 +3,16 @@ import hashlib
 from termcolor import colored
 
 
-sha1_hash = input('[*] Enter SHA-1 Hash value: ')
-password_list = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8')
+def sha1_bruteforce():
+    sha1_hash = input('[*] Enter SHA-1 Hash value: ')
+    password_list = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8')
 
-for password in password_list.split('\n'):
-    hashed = hashlib.sha1(bytes(password, 'utf-8')).hexdigest()
-    if hashed == sha1_hash:
-        print(colored('[+] The password is: ' + str(password), 'green'))
-        quit()
-    else:
-        print(colored('[-] Password ' + str(password) + ' does not match, trying next...', 'red'))
+    for password in password_list.split('\n'):
+        hashed = hashlib.sha1(bytes(password, 'utf-8')).hexdigest()
+        if hashed == sha1_hash:
+            print(colored('[+] The password is: ' + str(password), 'green'))
+            quit()
+        else:
+            print(colored('[-] Password ' + str(password) + ' does not match, trying next...', 'red'))
 
-print('Password not in the list')
+    print('Password not in the list')
